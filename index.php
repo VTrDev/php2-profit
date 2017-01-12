2f54d2500d9084da524347e29361a94176a5068c
Vladimir Trusov <trusov.public@gmail.com>
2017-01-13 00:26:05 +0300

    ! ActiveRecord

--- index.php
+++ index.php
@@ -1,17 +1,10 @@
 <?php
 
-use App\Models\HasEmail;
 use App\Models\User;
 
 require __DIR__ . '/autoload.php';
 
-$users = User::findAll();
-
-//var_dump($users);
-
-function sendEmail(HasEmail $user, string $message)
-{
-    echo 'Почта уходит на ' . $user->email;
-}
-
-sendEmail($users[0], 'Hello!');
+$user = new User();
+$user->name = 'Vasya';
+$user->email = 'v@pupkin.ru';
+$user->insert();
