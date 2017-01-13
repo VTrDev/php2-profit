68e379aec89110b282d32bc7140d26c0753763c0
Vladimir Trusov <trusov.public@gmail.com>
2017-01-13 21:17:17 +0300

    + Буфер вывода

--- App/View.php
+++ App/View.php
@@ -20,8 +20,21 @@ class View
      * 
      * @param $template string Путь к шаблону
      */
+    public function render($template)
+    {
+        ob_start();
+        include $template;
+        $content = ob_get_contents();
+        ob_end_clean();
+        return $content;
+    }
+    
+    /**
+     * 
+     * @param $template string Путь к шаблону
+     */
     public function display($template)
     {
-        include $template;
+        echo $this->render($template);
     }
 }
--- index.php
+++ index.php
@@ -3,6 +3,8 @@
 require __DIR__ . '/autoload.php';
 
 $view = new \App\View;
+
 $view->title = 'Мой крутой сайт!';
 $view->users = \App\Models\User::findAll();
-$view->display(__DIR__ . '/App/templates/index.php');
+
+$view->render(__DIR__ . '/App/templates/index.php');
