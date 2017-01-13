335539a71599d9a6019395bb88cc7483f33e1a32
Vladimir Trusov <trusov.public@gmail.com>
2017-01-13 22:21:51 +0300

    + Использование $$ во View. Применение SPL

--- App/View.php
+++ App/View.php
@@ -2,7 +2,7 @@
 
 namespace App;
 
-class View 
+class View implements \Countable
 {
     protected $data = [];
     
@@ -23,6 +23,9 @@ class View
     public function render($template)
     {
         ob_start();
+        foreach ($this->data as $prop => $value) {
+            $$prop = $value;
+        }
         include $template;
         $content = ob_get_contents();
         ob_end_clean();
@@ -37,4 +40,19 @@ class View
     {
         echo $this->render($template);
     }
+
+    /**
+     * (PHP 5 &gt;= 5.1.0, PHP 7)<br/>
+     * Count elements of an object
+     * @link http://php.net/manual/en/countable.count.php
+     * @return int The custom count as an integer.
+     * </p>
+     * <p>
+     * The return value is cast to an integer.
+     */
+    public function count(): int 
+    {
+        return count($this->data);
+    }
+
 }
--- index.php
+++ index.php
@@ -7,4 +7,7 @@ $view = new \App\View;
 $view->title = 'Мой крутой сайт!';
 $view->users = \App\Models\User::findAll();
 
-$view->render(__DIR__ . '/App/templates/index.php');
+//echo count($view);
+
+
+$view->display(__DIR__ . '/App/templates/index.php');
