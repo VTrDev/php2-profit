deb5152b7a771dd9343ec7ec60ce5ea62a76f004
Vladimir Trusov <trusov.public@gmail.com>
2017-01-14 22:05:30 +0300

    + ДЗ-3 ленивые логические выражения

--- test4.php
+++ test4.php
@@ -1,9 +1,27 @@
 <?php
 
 require __DIR__ . '/autoload.php';
+/*
+$authors = \App\Models\Author::findAll();
+var_dump($authors); 
+ */
 
-//$authors = \App\Models\Author::findAll();
-//var_dump($authors);
-
+/*
 $news = \App\Models\News::findAll();
-var_dump($news[0]->author);
+var_dump($news[0]->author); 
+*/
+
+
+
+function first() {
+    echo ' First ';
+    return false;
+}
+
+function second() {
+    echo ' Second ';
+    return true;
+}
+
+var_dump(first() && second()); // First bool(false)
+var_dump(second() || first()); // Second bool(true)
