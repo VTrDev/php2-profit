513b398239d8b3d497653a104a85c72040cb5014
Vladimir Trusov <trusov.public@gmail.com>
2017-01-15 11:49:35 +0300

    + Система контроллеров (еще одна страница)

--- App/Controllers/News.php
+++ App/Controllers/News.php
@@ -35,4 +35,11 @@ class News
         $this->view->news = \App\Models\News::findAll();
         $this->view->display(__DIR__ . '/../templates/index.php');
     }
+    
+    protected function actionOne()
+    {
+        $id = (int)$_GET['id'];
+        $this->view->article = \App\Models\News::findById($id);
+        $this->view->display(__DIR__ . '/../templates/one.php');
+    }
 }
