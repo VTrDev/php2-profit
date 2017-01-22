1fccbf1b35a98a88f5211a7e5fc6bcea4cb28349
Vladimir Trusov <trusov.public@gmail.com>
2017-01-22 22:31:13 +0300

    + spl_autoload_register

--- autoload.php
+++ autoload.php
@@ -3,8 +3,26 @@
 /** 
  * App\Models\User => ./App/Models/User.php
  */
+/*
 function __autoload($class)
 {
     require __DIR__ . '/' . str_replace('\\', '/', $class) . '.php';
+} 
+ */
+
+function my_app_autoload($class)
+{
+    $filename = __DIR__ . '/' . str_replace('\\', '/', $class) . '.php';
+    if (file_exists($filename)) {
+        include $filename;
+    }    
 }
 
+spl_autoload_register('my_app_autoload');
+
+spl_autoload_register(function ($class) {
+    $filename = __DIR__ . '/' . str_replace(['\\', 'App'], ['/', 'lib'], $class) . '.php';
+    if (file_exists($filename)) {
+        include $filename;
+    }    
+});
